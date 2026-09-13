bf64feea569f428faa6b81f78592b2070963a891
Iskander <bebrakebaba@gmail.com>
2026-09-13 19:02:21 +0500

    Add client demo

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,23 @@
+public class Main {
 
+    public static void main(String[] args) {
+
+        TravelPackageDirector director = new TravelPackageDirector();
+
+        TravelPackage budgetPackage = director.buildBudgetPackage(
+                new BudgetTravelPackageBuilder()
+        );
+
+        TravelPackage luxuryPackage = director.buildLuxuryPackage(
+                new LuxuryTravelPackageBuilder()
+        );
+
+        System.out.println("Budget Travel Package:");
+        System.out.println(budgetPackage);
+
+        System.out.println();
+
+        System.out.println("Luxury Travel Package:");
+        System.out.println(luxuryPackage);
+    }
+}
